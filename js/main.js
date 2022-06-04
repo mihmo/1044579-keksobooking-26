fbca4086b39aad0014fd0151f8186a1d8a91614e
Mykhailo Movchan <mihmov@gmail.com>
2022-06-04 19:20:51 +0100

    исправил ошибки, закоментировал мои тесты

--- js/main.js
+++ js/main.js
@@ -12,22 +12,23 @@ function getRandomFloatInclusive(min, max, decimal) {
 }
 
 function checkNumbers(min, max) {
-  return (min >= 0 && max >= 0 && min <= max) ? true : false;
+  return !!((min >= 0 && max >= 0 && min <= max));
+  // return (min >= 0 && max >= 0 && min <= max) ? true : false;
   // Линтер ругается: Unnecessary use of boolean literals in conditional expression.
   // Предлагает заменить на return !!((min >= 0 && max >= 0 && min <= max))
 }
 
 // Проверка выполнения
 
-let resultsArray = [];
+const resultsArray = [];
 // линтер ругается: 'resultsArray' is never reassigned. Use 'const' instead.
 // почему так происходит, ведь я использую его в цикле и передаю его далее в alert?
 
 for (let i = 0; i < 10; i++) {
   resultsArray.push(getRandomFloatInclusive(1, 10, 3));
-  // resultsArray.push(getRandomIntInclusive(1, 10));
+  resultsArray.push(getRandomIntInclusive(1, 10));
 }
 
-alert(resultsArray);
+// console.log(resultsArray);
 
 
