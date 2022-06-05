575a005c3670a3dc7ee46bea575dbe535fd20ca0
Mykhailo Movchan <mihmov@gmail.com>
2022-06-05 17:52:01 +0100

    создал константы, заменил функции на стрелочные

--- js/main.js
+++ js/main.js
@@ -1,34 +1,26 @@
-// функцию скопировал отсюда: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const MIN_VALUE = 1;
+const MAX_VALUE = 10;
+const TOTAL_NUMBERS = 10;
+const FLOAT_NUMBERS = 2;
 
-function getRandomIntInclusive(min, max) {
+const checkNumbers = (min, max)  => !!((min >= 0 && max >= 0 && min <= max));
+
+// функцию скопировал отсюда: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   return checkNumbers(min, max) ? Math.floor(Math.random() * (max - min + 1)) + min : 'Указаны не верные параметры'; // Максимум и минимум включаются
-}
+};
 
-function getRandomFloatInclusive(min, max, decimal) {
-  return checkNumbers(min, max) ? (Math.random() * (max - min) + min).toFixed(decimal) : 'Указаны не верные параметры'; // Максимум и минимум включаются
-}
-
-function checkNumbers(min, max) {
-  return !!((min >= 0 && max >= 0 && min <= max));
-  // return (min >= 0 && max >= 0 && min <= max) ? true : false;
-  // Линтер ругается: Unnecessary use of boolean literals in conditional expression.
-  // Предлагает заменить на return !!((min >= 0 && max >= 0 && min <= max))
-}
+const getRandomFloatInclusive = (min, max, decimal) => checkNumbers(min, max) ? (Math.random() * (max - min) + min).toFixed(decimal) : 'Указаны не верные параметры'; // Максимум и минимум включаются
 
 // Проверка выполнения
-
 const resultsArray = [];
-// линтер ругается: 'resultsArray' is never reassigned. Use 'const' instead.
-// почему так происходит, ведь я использую его в цикле и передаю его далее в alert?
-
-for (let i = 0; i < 10; i++) {
-  resultsArray.push(getRandomFloatInclusive(1, 10, 3));
-  resultsArray.push(getRandomIntInclusive(1, 10));
+for (let i = 0; i < TOTAL_NUMBERS; i++) {
+  resultsArray.push(getRandomFloatInclusive(MIN_VALUE, MAX_VALUE, FLOAT_NUMBERS));
+  resultsArray.push(getRandomIntInclusive(MIN_VALUE, MAX_VALUE));
 }
 
+// console.log(checkNumbers(MIN_VALUE, MAX_VALUE));
 // console.log(resultsArray);
-
-
